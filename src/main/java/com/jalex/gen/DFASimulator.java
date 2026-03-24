d636585d84910b0781759964795515fb22fe87ad
Anaru03 <dele22428@uvg.edu.gt>
2026-03-24 09:13:20 -0600

    Implementar NFA y DFA completos mediante Thompson y Subset Construction

---
+++ src/main/java/com/jalex/gen/DFASimulator.java
@@ -0,0 +1,50 @@
+package com.jalex.gen;
+
+import com.jalex.dfa.DFA;
+import com.jalex.dfa.DFAState;
+
+public class DFASimulator {
+
+    public void simulate(DFA dfa, String input) {
+
+        int i = 0;
+
+        while (i < input.length()) {
+
+            DFAState current = dfa.startState;
+            DFAState lastAccepting = null;
+            int lastIndex = i;
+
+            int j = i;
+
+            while (j < input.length()) {
+
+                String symbol = String.valueOf(input.charAt(j));
+
+                DFAState next = current.getTransition(symbol);
+
+                if (next == null) break;
+
+                current = next;
+
+                if (current.isAccepting) {
+                    lastAccepting = current;
+                    lastIndex = j + 1;
+                }
+
+                j++;
+            }
+
+            if (lastAccepting != null) {
+                String lexeme = input.substring(i, lastIndex);
+
+                System.out.println("TOKEN → " + lexeme + " | acción: " + lastAccepting.action);
+
+                i = lastIndex;
+            } else {
+                System.out.println("ERROR léxico en: " + input.charAt(i));
+                i++;
+            }
+        }
+    }
+}
